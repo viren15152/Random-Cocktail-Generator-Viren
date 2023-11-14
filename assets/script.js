36375f683b48378247197af2ac595477ca562ebb
Viren <viren15152@gmail.com>
2023-11-14 04:59:39 +0000

    I've added the rest of the JavaScript so that the page functions as intended

--- assets/script.js
+++ assets/script.js
@@ -18,8 +18,41 @@ document.getElementById("generateButton").addEventListener("click", function () 
       });
   });
 
-  //I wrote this block of code to display the data on the page 
-  function displayCocktail(data) {
-    const cocktail = data.drinks[0];
-    const cocktailDiv = document.getElementById("cocktail");
+    //This section will display the data on the page 
+  
+    function displayCocktail(data) {
+        const cocktail = data.drinks[0];
+        const cocktailDiv = document.getElementById("cocktail");
+      
+        //This section will clear previous content from the page 
+        cocktailDiv.innerHTML = "";
+      
+        const cocktailName = cocktail.strDrink;
+        const heading = document.createElement("h1");
+        heading.innerHTML = cocktailName;
+        cocktailDiv.appendChild(heading);
+      
+        const cocktailImg = document.createElement("img");
+        cocktailImg.src = cocktail.strDrinkThumb;
+        cocktailDiv.appendChild(cocktailImg);
+        document.body.style.backgroundImage = "url('" + cocktail.strDrinkThumb + "')";
+      
+        const cocktailIngredients = document.createElement("ul");
+        cocktailDiv.appendChild(cocktailIngredients);
+      
+        // This section will loop through ingredients and measurements
+        for (let i = 1; i <= 15; i++) {
+          const ingredient = cocktail[`strIngredient${i}`];
+          const measurement = cocktail[`strMeasure${i}`];
+      
+          if (ingredient) {
+            const listItem = document.createElement("li");
+            listItem.innerHTML = measurement ? `${measurement} ${ingredient}` : ingredient;
+            cocktailIngredients.appendChild(listItem);
+          } else {
+            break; // This will exit the loop if no more ingredients
+          }
+        }
+      }
+
   
